be6855e7a2f5935a1c9aac910ade8bd5086fb8e5
bigyem <adeniranyemi96@gmail.com>
2023-03-20 18:37:20 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+int main(void)
+{
+    char c = 'a';
+
+    // Print the alphabet using a loop
+    while (c <= 'z') {
+        putchar(c);
+        c++;
+    }
+
+    // Print a new line character
+    putchar('\n');
+
+    return 0;
+}
